e6fbe6c7099ecd4be60e3ce884ce495204ec0929
Steve Smith <stevejsmith567@gmail.com>
2017-03-28 11:04:52 +0100

    Data previewed

--- TitanicAnalysis.py
+++ TitanicAnalysis.py
@@ -51,3 +51,161 @@ print(test_df.columns.values)
     
 # Continuous: Age, Fare. Discrete: SibSp, Parch.
 
+
+#--------------------------------
+
+# Previewing the data
+
+train_df.head()
+"""
+   PassengerId  Survived  Pclass  \
+0            1         0       3   
+1            2         1       1   
+2            3         1       3   
+3            4         1       1   
+4            5         0       3   
+
+                                                Name     Sex   Age  SibSp  \
+0                            Braund, Mr. Owen Harris    male  22.0      1   
+1  Cumings, Mrs. John Bradley (Florence Briggs Th...  female  38.0      1   
+2                             Heikkinen, Miss. Laina  female  26.0      0   
+3       Futrelle, Mrs. Jacques Heath (Lily May Peel)  female  35.0      1   
+4                           Allen, Mr. William Henry    male  35.0      0   
+
+   Parch            Ticket     Fare Cabin Embarked  
+0      0         A/5 21171   7.2500   NaN        S  
+1      0          PC 17599  71.2833   C85        C  
+2      0  STON/O2. 3101282   7.9250   NaN        S  
+3      0            113803  53.1000  C123        S  
+4      0            373450   8.0500   NaN        S  
+"""
+
+# Mixed data types
+
+# Ticket is a mix of numeric and alphanumeric data types. Cabin is alphanumeric.
+
+# Which features may contain errors or typos?
+
+# Name feature may contain errors or typos as there are several ways used to describe a name including titles, round brackets, and quotes used for alternative or short names.
+
+train_df.tail()
+
+"""
+     PassengerId  Survived  Pclass                                      Name  \
+886          887         0       2                     Montvila, Rev. Juozas   
+887          888         1       1              Graham, Miss. Margaret Edith   
+888          889         0       3  Johnston, Miss. Catherine Helen "Carrie"   
+889          890         1       1                     Behr, Mr. Karl Howell   
+890          891         0       3                       Dooley, Mr. Patrick   
+
+        Sex   Age  SibSp  Parch      Ticket   Fare Cabin Embarked  
+886    male  27.0      0      0      211536  13.00   NaN        S  
+887  female  19.0      0      0      112053  30.00   B42        S  
+888  female   NaN      1      2  W./C. 6607  23.45   NaN        S  
+889    male  26.0      0      0      111369  30.00  C148        C  
+890    male  32.0      0      0      370376   7.75   NaN        Q  
+"""
+
+# Which features contain blank, null or empty values?
+
+# Cabin > Age > Embarked features contain a number of null values in that order for the training dataset.
+
+# Cabin > Age are incomplete in case of test dataset.
+
+
+# variable types
+
+train_df.info()
+"""
+<class 'pandas.core.frame.DataFrame'>
+RangeIndex: 891 entries, 0 to 890
+Data columns (total 12 columns):
+PassengerId    891 non-null int64
+Survived       891 non-null int64
+Pclass         891 non-null int64
+Name           891 non-null object
+Sex            891 non-null object
+Age            714 non-null float64
+SibSp          891 non-null int64
+Parch          891 non-null int64
+Ticket         891 non-null object
+Fare           891 non-null float64
+Cabin          204 non-null object
+Embarked       889 non-null object
+dtypes: float64(2), int64(5), object(5)
+memory usage: 83.6+ KB
+"""
+
+test_df.info()
+"""
+<class 'pandas.core.frame.DataFrame'>
+RangeIndex: 418 entries, 0 to 417
+Data columns (total 11 columns):
+PassengerId    418 non-null int64
+Pclass         418 non-null int64
+Name           418 non-null object
+Sex            418 non-null object
+Age            332 non-null float64
+SibSp          418 non-null int64
+Parch          418 non-null int64
+Ticket         418 non-null object
+Fare           417 non-null float64
+Cabin          91 non-null object
+Embarked       418 non-null object
+dtypes: float64(2), int64(4), object(5)
+memory usage: 36.0+ KB
+"""
+
+# 7 integer/float features in the train dataframe
+
+# 6 integer/float features in the test dataframe
+
+# 5 features are strings
+
+# Survived missing in the test dataframe, this is the feature we wish to predict
+
+
+# How representative is the training dataset of the actual problem domain?
+
+train_df.describe()
+"""
+       PassengerId    Survived      Pclass         Age       SibSp  \
+count   891.000000  891.000000  891.000000  714.000000  891.000000   
+mean    446.000000    0.383838    2.308642   29.699118    0.523008   
+std     257.353842    0.486592    0.836071   14.526497    1.102743   
+min       1.000000    0.000000    1.000000    0.420000    0.000000   
+25%     223.500000    0.000000    2.000000   20.125000    0.000000   
+50%     446.000000    0.000000    3.000000   28.000000    0.000000   
+75%     668.500000    1.000000    3.000000   38.000000    1.000000   
+max     891.000000    1.000000    3.000000   80.000000    8.000000   
+
+            Parch        Fare  
+count  891.000000  891.000000  
+mean     0.381594   32.204208  
+std      0.806057   49.693429  
+min      0.000000    0.000000  
+25%      0.000000    7.910400  
+50%      0.000000   14.454200  
+75%      0.000000   31.000000  
+max      6.000000  512.329200  
+"""
+
+# distribution of categorical features:
+    # quick sort in excel shows
+    
+# Names are unique
+# sex variable, Male/Female, 65% male
+# Cabin values have duplicates, perhaps several passengers shared a cabin
+
+# embarked takes three possible values relating to the port of embarkation
+
+# S(outhamption) is most
+
+# Ticket feature has high ratio (22%) of duplicate values
+
+
+                            
+# correlating
+
+
+                        
